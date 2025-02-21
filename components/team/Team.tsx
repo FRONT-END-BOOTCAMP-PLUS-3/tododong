86a25712483bba53b31e72eb40a7e50239fa0515
jisooyoon <yoonmallang@gmail.com>
2025-02-21 17:04:37 +0900

    ♻️ refactor: props nickname 제거, name으로 통합

--- components/team/Team.tsx
+++ components/team/Team.tsx
@@ -1,18 +1,13 @@
 import styles from './Team.module.scss';
 import Image from 'next/image';
 
-type TeamProps = { logoSrc: string; name?: string; nickname?: string };
+type TeamProps = { logoSrc: string; name: string };
 
-const Team = ({ logoSrc, name, nickname }: TeamProps) => {
+const Team = ({ logoSrc, name }: TeamProps) => {
   return (
     <div className={styles.team}>
-      <Image
-        src={logoSrc}
-        width={40}
-        height={40}
-        alt={`${name || nickname} 로고`}
-      />
-      <p>{name || nickname}</p>
+      <Image src={logoSrc} width={40} height={40} alt={`${name} 로고`} />
+      <p>{name}</p>
     </div>
   );
 };
@@ -20,9 +15,6 @@ const Team = ({ logoSrc, name, nickname }: TeamProps) => {
 export default Team;
 
 /*
-사용 예시 1
+사용 예시
 <Team logoSrc="https://" name="Atlanta Hawks" />
-
-사용 예시 2
-<Team logoSrc="https://" nickname="Hawks" />
 */
