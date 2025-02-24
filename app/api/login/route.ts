06b616cc3adaca11afe4cfda99c4b1affaa963ac
JoowonJang <wndnjs5248@hanmail.net>
2025-02-24 11:04:21 +0900

    ✨ feat: login 페이지 UI완성
    - fetch 요청은 route.ts에 더미데이터로 채워둠

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,1 +1,11 @@
-export const GET = async () => {};
+import { NextRequest, NextResponse } from 'next/server';
+
+export const POST = async (req: NextRequest) => {
+  const { email, password } = await req.json();
+
+  console.log(email, password);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return NextResponse.json({ error: '로그인 실패' }, { status: 400 });
+  // return NextResponse.json({ message: '로그인 성공' }, { status: 200 });
+};
