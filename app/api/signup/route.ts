8c56507bf6126f690013c6ca7176cc7e7148dff7
JoowonJang <wndnjs5248@hanmail.net>
2025-02-24 11:03:44 +0900

    ✨ feat: signup 페이지 UI 완성
    - fetch 요청은 route.ts에 더미데이터로 채워둠

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,1 +1,11 @@
-export const GET = async () => {};
+import { NextRequest, NextResponse } from 'next/server';
+
+export const POST = async (req: NextRequest) => {
+  const { email, password, nickName } = await req.json();
+
+  console.log(email, password, nickName);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return NextResponse.json({ message: '회원가입 성공' }, { status: 200 });
+  // return NextResponse.json({ error: '회원가입 실패' }, { status: 400 });
+};
