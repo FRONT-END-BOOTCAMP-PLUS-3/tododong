3848a415b8697f4a058d7bb9b1dc0bb6e9aacf7c
JoowonJang <wndnjs5248@hanmail.net>
2025-02-25 19:38:55 +0900

    ♻️ refactor: LoginForm을 FormAction을 사용하도록 변경
    - useActionState 사용
    - onSubmit에서 처리하던 로그인 요청을 loginProc으로 이동

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,9 +3,11 @@ import { NextRequest, NextResponse } from 'next/server';
 export const POST = async (req: NextRequest) => {
   const { email, password } = await req.json();
 
-  console.log(email, password);
+  console.log('로그인 api요청:', { email, password });
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  return NextResponse.json({ error: '로그인 실패' }, { status: 400 });
-  // return NextResponse.json({ message: '로그인 성공' }, { status: 200 });
+  const token = 'asdf';
+
+  return NextResponse.json({ token }, { status: 200 });
+  // return NextResponse.json({ error: '로그인 실패' }, { status: 400 });
 };
