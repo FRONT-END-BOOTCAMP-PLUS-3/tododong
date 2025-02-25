4b0eef2610d4f52ed4e94f1197ffe8c1770f8152
gayeongogo <rkdus1357@gmail.com>
2025-02-25 15:34:14 +0900

    💄 style: 데이터 형식 수정

--- app/(anon)/game/[game-id]/components/game-status/GameStatusSection.tsx
+++ app/(anon)/game/[game-id]/components/game-status/GameStatusSection.tsx
@@ -2,85 +2,80 @@ import GameStatusTag from '@/components/game-status-tag/GameStatusTag';
 import styles from './GameStatusSection.module.scss';
 import Team from '@/components/team/Team';
 
-const GameData = [
-  {
-    id: 14902,
-    date: {
-      start: '2025-02-21T00:00:00.000Z',
-      end: null,
-    },
-    teams: {
-      visitors: {
-        id: 19,
-        name: 'Memphis Grizzlies',
-        code: 'MEM',
-        logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/Memphis_Grizzlies.svg/1200px-Memphis_Grizzlies.svg.png',
-      },
-      home: {
-        id: 15,
-        name: 'Indiana Pacers',
-        code: 'IND',
-        logo: 'https://upload.wikimedia.org/wikipedia/fr/thumb/c/cf/Pacers_de_l%27Indiana_logo.svg/1180px-Pacers_de_l%27Indiana_logo.svg.png',
-      },
-    },
-    scores: {
-      visitors: {
-        linescore: ['33', '26', '23', ''],
-        points: 82,
-      },
-      home: {
-        linescore: ['28', '50', '13', ''],
-        points: 91,
-      },
-    },
+const dto = {
+  id: 14903,
+  game: {
+    startDate: '2024.02.25',
+    startTime: '10:00 AM KST',
+    arena: 'Wells Fargo Center',
+    status: 'scheduled',
   },
-];
-
+  home: {
+    name: 'Chicago Bulls',
+    code: 'CHI',
+    logo: 'https://upload.wikimedia.org/wikipedia/fr/thumb/d/d1/Bulls_de_Chicago_logo.svg/1200px-Bulls_de_Chicago_logo.svg.png',
+    linescore: ['21', '35', '', ''],
+    points: 56,
+  },
+  away: {
+    name: 'Atlanta Hawks',
+    nickname: 'Hawks',
+    code: 'ATL',
+    logo: 'https://upload.wikimedia.org/wikipedia/fr/e/ee/Hawks_2016.png',
+    linescore: ['33', '23', '', ''],
+    points: 56,
+  },
+};
 const GameStatusSection = () => {
-  const data = GameData[0];
   return (
     <section className={styles.container}>
       <div className={styles.teamWrap}>
-        <Team
-          logoSrc={data.teams.visitors.logo}
-          name={data.teams.visitors.code}
-        />
+        <Team logoSrc={dto.away.logo} name={dto.away.code} />
       </div>
-      <div>
-        <div className={styles.matchStatus}>
-          {/* date.start가 현재 일시와 동일하거나 그 이상이면 'live', 이전이면 '예정' end보다 이후면 '종료',*/}
-          <p>30</p>
-          <GameStatusTag size="lg" status="live" />
-          <p>30</p>
+      {dto.game.status === 'scheduled' ? (
+        <div className={styles.matchScheduled}>
+          <p>{dto.game.arena}</p>
+          <p>{dto.game.startDate}</p>
+          <p>{dto.game.startTime}</p>
         </div>
-        <table className={styles.matchTable}>
-          <thead>
-            <tr>
-              <th>팀</th>
-              <th>1Q</th>
-              <th>2Q</th>
-              <th>3Q</th>
-              <th>4Q</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{data.teams.visitors.name}</td>
-              {data.scores.visitors.linescore.map((score, index) => (
-                <td key={index}>{score || '-'}</td>
-              ))}
-            </tr>
-            <tr>
-              <td>{data.teams.home.name}</td>
-              {data.scores.home.linescore.map((score, index) => (
-                <td key={index}>{score || '-'}</td>
-              ))}
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      ) : (
+        <div>
+          <div className={styles.matchStatus}>
+            {/* date.start가 현재 일시와 동일하거나 그 이상이면 'live', 이전이면 '예정' end보다 이후면 '종료',*/}
+            <p>{dto.away.points}</p>
+            <GameStatusTag size="lg" status={dto.game.status} />
+            <p>{dto.home.points}</p>
+          </div>
+          <table className={styles.matchTable}>
+            <thead>
+              <tr>
+                <th>팀</th>
+                <th>1Q</th>
+                <th>2Q</th>
+                <th>3Q</th>
+                <th>4Q</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{dto.away.name}</td>
+                {dto.away.linescore.map((score, index) => (
+                  <td key={index}>{score || '-'}</td>
+                ))}
+              </tr>
+              <tr>
+                <td>{dto.home.name}</td>
+                {dto.home.linescore.map((score, index) => (
+                  <td key={index}>{score || '-'}</td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
+
       <div className={styles.teamWrap}>
-        <Team logoSrc={data.teams.home.logo} name={data.teams.home.code} />
+        <Team logoSrc={dto.home.logo} name={dto.home.code} />
       </div>
     </section>
   );
