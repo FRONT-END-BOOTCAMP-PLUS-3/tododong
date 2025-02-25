95fb55b8842a9410318b016e5cdd9fa40623b5d4
JoowonJang <wndnjs5248@hanmail.net>
2025-02-25 19:38:55 +0900

    ♻️ refactor: LoginForm을 FormAction을 사용하도록 변경
    - useActionState 사용
    - onSubmit에서 처리하던 로그인 요청을 loginProc으로 이동

--- app/(anon)/login/components/LoginForm.tsx
+++ app/(anon)/login/components/LoginForm.tsx
@@ -2,21 +2,22 @@
 
 import AuthInput from '@/components/auth-input/AuthInput';
 import Icon from '@/components/icon/Icon';
-import { fetcher } from '@/utils';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
+import React, { useActionState, useState } from 'react';
+import { loginProc } from '../actions/loginProc';
 import styles from './LoginForm.module.scss';
 
-const LoginForm = () => {
-  const router = useRouter();
+const initialState = {
+  message: '',
+};
 
+const LoginForm = () => {
   /* --------------------------------- states --------------------------------- */
+  const [state, formAction, pending] = useActionState(loginProc, initialState);
+
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
-  const [isInvalid, setIsInvalid] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
   const isFormFilled = Object.values(formData).every((value) => value.trim());
 
   /* -------------------------------- functions ------------------------------- */
@@ -25,34 +26,9 @@ const LoginForm = () => {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // 로그인 요청
-    try {
-      await fetcher(
-        '/api/login',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
-        },
-        setIsLoading
-      );
-
-      // 로그인 성공 시 홈으로 이동
-      router.push('/');
-    } catch (error) {
-      console.error(error);
-      // 로그인 실패 시 에러 메시지 표시
-      setIsInvalid(true);
-    }
-  };
-
   /* -------------------------------- rendering ------------------------------- */
   return (
-    <form onSubmit={handleSubmit} aria-label="로그인" className={styles.form}>
+    <form action={formAction} aria-label="로그인" className={styles.form}>
       <AuthInput
         type="email"
         placeholder="이메일"
@@ -69,17 +45,15 @@ const LoginForm = () => {
       />
 
       {/* 오류 메세지 */}
-      {isInvalid && (
+      {state.message && (
         <div role="alert" className={styles.message}>
           <Icon id="info" width={16} color="#fa2f2f" aria-hidden />
-          아이디 또는 비밀번호가 잘못되었습니다.
-          <br />
-          아이디와 비밀번호를 정확히 입력해 주세요.
+          {state.message}
         </div>
       )}
 
-      <button type="submit" disabled={!isFormFilled || isLoading}>
-        {isLoading ? '로그인 중...' : '로그인'}
+      <button type="submit" disabled={!isFormFilled || pending}>
+        {pending ? '로그인 중...' : '로그인'}
       </button>
     </form>
   );
