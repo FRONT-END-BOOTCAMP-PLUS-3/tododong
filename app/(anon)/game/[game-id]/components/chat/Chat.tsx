a97e8289348ff3d82e42c254fe9f86ba7f3b31e6
gayeongogo <rkdus1357@gmail.com>
2025-02-25 15:27:43 +0900

    🎨 design: 정렬 방식 수정

--- app/(anon)/game/[game-id]/components/chat/Chat.tsx
+++ app/(anon)/game/[game-id]/components/chat/Chat.tsx
@@ -3,8 +3,10 @@ import styles from './Chat.module.scss';
 const Chat = () => {
   return (
     <div className={styles.container}>
-      <p>닉네임</p>
-      <p>채팅 내용</p>
+      <span>강백호</span>
+      <span>
+        ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+      </span>
     </div>
   );
 };
