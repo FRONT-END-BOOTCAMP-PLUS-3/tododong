503f70145e7046faed477af911e88cf59f5a3e77
seungsu-K <kss095@naver.com>
2025-02-26 15:13:03 +0900

    🔧 chore : 실시간 중계 임시 데이터

--- app/api/game/[game-id]/play-by-play/route.ts
+++ app/api/game/[game-id]/play-by-play/route.ts
@@ -1,1 +1,13 @@
-export const GET = async () => {};
+import { mockData } from '@/app/(anon)/game/[game-id]/play-by-play/mockData';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const POST = async (req: NextRequest) => {
+  const { gameId } = await req.json();
+
+  const result = mockData;
+
+  //   await new Promise((resolve) => setTimeout(resolve, 1000));
+  //   return NextResponse.json({ error: '로그인 실패' }, { status: 400 });
+
+  return NextResponse.json(result, { status: 200 });
+};
