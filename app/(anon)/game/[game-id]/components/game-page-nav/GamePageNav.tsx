e42c8bb745f3d9a2ca8846a834a251563bfb0a99
gayeongogo <rkdus1357@gmail.com>
2025-02-27 19:16:02 +0900

    💄 style: <Link>를 감싸는 <ul>, <li> 추가

--- app/(anon)/game/[game-id]/components/game-page-nav/GamePageNav.tsx
+++ app/(anon)/game/[game-id]/components/game-page-nav/GamePageNav.tsx
@@ -8,31 +8,39 @@ const GamePageNav = ({ gameId }: { gameId: string }) => {
   const pathname = usePathname();
 
   return (
-    <nav className={styles.navContainer}>
-      <Link
-        href={`/game/${gameId}/videos`}
-        className={`${styles.link} ${
-          pathname === `/game/${gameId}/videos` ? styles.active : ''
-        }`}
-      >
-        영상
-      </Link>
-      <Link
-        href={`/game/${gameId}/box-score`}
-        className={`${styles.link} ${
-          pathname === `/game/${gameId}/box-score` ? styles.active : ''
-        }`}
-      >
-        선수 기록
-      </Link>
-      <Link
-        href={`/game/${gameId}/play-by-play`}
-        className={`${styles.link} ${
-          pathname === `/game/${gameId}/play-by-play` ? styles.active : ''
-        }`}
-      >
-        실시간 중계
-      </Link>
+    <nav>
+      <ul className={styles.navContainer}>
+        <li>
+          <Link
+            href={`/game/${gameId}/videos`}
+            className={`${styles.link} ${
+              pathname === `/game/${gameId}/videos` ? styles.active : ''
+            }`}
+          >
+            영상
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={`/game/${gameId}/box-score`}
+            className={`${styles.link} ${
+              pathname === `/game/${gameId}/box-score` ? styles.active : ''
+            }`}
+          >
+            선수 기록
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={`/game/${gameId}/play-by-play`}
+            className={`${styles.link} ${
+              pathname === `/game/${gameId}/play-by-play` ? styles.active : ''
+            }`}
+          >
+            실시간 중계
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
