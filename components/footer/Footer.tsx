6c2f78826c308612c3686d738568cbb4b143d50a
jisooyoon <yoonmallang@gmail.com>
2025-03-05 14:40:10 +0900

    🎨 design: 색상 변경

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -28,62 +28,55 @@ const Footer = () => {
   return (
     <>
       <footer className={styles.footer}>
-        <div className={styles.footerInner}>
-          <Image
-            src="/logo_footer.png"
-            width={65}
-            height={51}
-            alt="footer 로고"
-          />
-          <div>
-            <address>
-              상호명: 토도동 | 대표: 김승수 | 이메일: tododong@gmail.com
-              <br />
-              주소: 서울 중구 퇴계로 324, 성우빌딩 10층
-            </address>
-            <p>© tododong. All rights reserved.</p>
-          </div>
-          <ul className={styles.termWrapper}>
-            <li>
-              {user ? (
-                <button onClick={() => setIsModalOpen(true)}>로그아웃</button>
-              ) : (
-                <Link href="/login" className={styles.loginLink}>
-                  로그인
-                </Link>
-              )}
-            </li>
-            <li>
-              <Link href="">이용약관</Link>
-            </li>
-            <li>
-              <Link href="">광고문의</Link>
-            </li>
-            <li>
-              <Link href="">개인정보처리방침</Link>
-            </li>
-            <li>
-              <Link href="">공지사항</Link>
-            </li>
-          </ul>
-          <ul className={styles.socialList}>
-            <li>
-              <Link href="">
-                <Icon id="facebook" width={24} color="#000" />
-              </Link>
-            </li>
-            <li>
-              <Link href="">
-                <Icon id="instagram" width={24} color="#000" />
-              </Link>
-            </li>
-            <li>
-              <Link href="https://github.com/FRONT-END-BOOTCAMP-PLUS-3/tododong/tree/main">
-                <Icon id="github" width={24} color="#000" />
-              </Link>
-            </li>
-          </ul>
+        <Image src="/logo_sm.png" width={65} height={51} alt="footer 로고" />
+        <div>
+          <address>
+            상호명: 토도동 | 대표: 김승수 | 이메일: tododong@gmail.com
+            <br />
+            주소: 서울 중구 퇴계로 324, 성우빌딩 10층
+          </address>
+          <p>© tododong. All rights reserved.</p>
         </div>
+        <ul className={styles.termWrapper}>
+          <li>
+            {user ? (
+              <button onClick={() => setIsModalOpen(true)}>로그아웃</button>
+            ) : (
+              <Link href="/login" className={styles.loginLink}>
+                로그인
+              </Link>
+            )}
+          </li>
+          <li>
+            <Link href="">이용약관</Link>
+          </li>
+          <li>
+            <Link href="">광고문의</Link>
+          </li>
+          <li>
+            <Link href="">개인정보처리방침</Link>
+          </li>
+          <li>
+            <Link href="">공지사항</Link>
+          </li>
+        </ul>
+        <ul className={styles.socialList}>
+          <li>
+            <Link href="">
+              <Icon id="facebook" width={24} />
+            </Link>
+          </li>
+          <li>
+            <Link href="">
+              <Icon id="instagram" width={24} />
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/FRONT-END-BOOTCAMP-PLUS-3/tododong/tree/main">
+              <Icon id="github" width={24} />
+            </Link>
+          </li>
+        </ul>
       </footer>
       {isModalOpen && (
         <Modal
