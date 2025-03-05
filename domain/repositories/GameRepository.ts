694e695283cc22185192a18f6ab79781820f8f8f
seungsu-K <kss095@naver.com>
2025-03-05 11:33:18 +0900

    💄 style : 메서드 순서 변경

--- domain/repositories/GameRepository.ts
+++ domain/repositories/GameRepository.ts
@@ -2,6 +2,6 @@ import { Game } from '@prisma/client';
 
 export interface GameRepository {
   findAll: () => Promise<Game[]>;
-  saveGames?: (games: Game[]) => Promise<void>;
   findById: (id: string) => Promise<Game | null>;
+  saveGames?: (games: Game[]) => Promise<void>;
 }
