1f9974c318977bf4f666bef85e92903213045013
jisooyoon <yoonmallang@gmail.com>
2025-03-07 14:57:36 +0900

    🐛 fix: findByDate 메서드 옵셔널로 변경

--- domain/repositories/GameRepository.ts
+++ domain/repositories/GameRepository.ts
@@ -2,6 +2,6 @@ import { Game } from '@prisma/client';
 
 export interface GameRepository {
   findAll: () => Promise<Game[]>;
-  findByDate: (date: string) => Promise<Game[]>;
+  findByDate?: (date: string) => Promise<Game[]>;
   saveGames?: (games: Game[]) => Promise<void>;
 }
