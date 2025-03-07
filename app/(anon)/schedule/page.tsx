37f3c26a2bd918825fa2a3937df0762e0e379a01
jisooyoon <yoonmallang@gmail.com>
2025-03-07 14:40:11 +0900

    ♻️ refactor: 로딩 컴포넌트 제거 및 불필요한 상태 관리 정리

--- app/(anon)/schedule/page.tsx
+++ app/(anon)/schedule/page.tsx
@@ -9,7 +9,6 @@ import GameCard from './components/game-card/GameCard';
 import { ScheduledGameDto } from '@/application/usecases/schedule/dto/ScheduledGameDto';
 
 const Schedule = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [scheduledGames, setScheduledGames] = useState<ScheduledGameDto[]>();
   const date = useDateStore((state) => state.date);
 
@@ -19,9 +18,7 @@ const Schedule = () => {
     const fetchScheduledGames = async () => {
       try {
         const response = await fetcher<ScheduledGameDto[]>(
-          `${process.env.NEXT_PUBLIC_API_URL}/schedule/${date}`,
-          {},
-          setIsLoading
+          `${process.env.NEXT_PUBLIC_API_URL}/schedule/${date}`
         );
 
         setScheduledGames(response);
@@ -37,7 +34,6 @@ const Schedule = () => {
     <>
       <DatePicker />
       <main>
-        {isLoading && <div style={{ textAlign: 'center' }}>로딩중...</div>}
         {scheduledGames && scheduledGames.length > 0 ? (
           <div className={styles.cardWrapper}>
             {scheduledGames.map((game) => (
