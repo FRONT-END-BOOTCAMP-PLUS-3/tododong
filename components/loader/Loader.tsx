a40e3ac52e3c82a637430f7404cb7e294e25af60
jisooyoon <yoonmallang@gmail.com>
2025-03-07 15:05:16 +0900

    ✨ feat: Loader 컴포넌트 추가

---
+++ components/loader/Loader.tsx
@@ -0,0 +1,31 @@
+import styles from './Loader.module.scss';
+import Image from 'next/image';
+
+type LoaderProps = {
+  className?: string;
+  size?: number;
+};
+
+const Loader = ({ className, size }: LoaderProps) => {
+  return (
+    <div className={`${styles.loaderContainer} ${className}`}>
+      <Image
+        src="/loader.png"
+        className={styles.loader}
+        width={size || 52}
+        height={size || 52}
+        alt="로딩 이미지"
+      />
+    </div>
+  );
+};
+
+export default Loader;
+
+/*
+사용 예시 1: (width), height 지정
+<Loader className={styles.loader} />
+
+사용 예시 2: 농구공 크기 조정 필요할 때
+<Loader className={styles.loader} size={20}/>
+*/
