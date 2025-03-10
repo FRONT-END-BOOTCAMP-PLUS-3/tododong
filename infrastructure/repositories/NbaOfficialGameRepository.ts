5652b8ba9a6ab123cf2833445d90a5fd650c1d4d
jisooyoon <yoonmallang@gmail.com>
2025-03-10 15:36:07 +0900

    💄 style: 필요없는 데이터 타입 삭제

--- infrastructure/repositories/NbaOfficialGameRepository.ts
+++ infrastructure/repositories/NbaOfficialGameRepository.ts
@@ -1,87 +1,27 @@
 import { GameRepository } from '@/domain/repositories/GameRepository';
 import { Game } from '@prisma/client';
 
-interface Broadcaster {
-  broadcasterScope?: string;
-  broadcasterMedia?: string;
-  broadcasterId?: number;
-  broadcasterDisplay?: string;
-  broadcasterAbbreviation?: string;
-  broadcasterDescription?: string;
-  tapeDelayComments?: string;
-  broadcasterVideoLink?: string;
-  broadcasterTeamId?: number;
-}
-
 interface GameDetail {
   gameId: string;
-  gameCode: string;
   gameStatus: number;
   gameStatusText: string;
-  gameSequence: number;
-  gameDateEst: string;
-  gameTimeEst: string;
-  gameDateTimeEst: string;
-  gameDateUTC: string;
-  gameTimeUTC: string;
   gameDateTimeUTC: string;
-  awayTeamTime: string;
-  homeTeamTime: string;
-  day: string;
-  monthNum: number;
-  weekNumber: number;
-  weekName: string;
   gameLabel: string;
-  gameSubLabel: string;
   arenaName: string;
-  arenaState: string;
-  arenaCity: string;
-  postponedStatus: string;
-  gameSubtype: string;
-  isNeutral: boolean;
-  broadcasters: {
-    nationalBroadcasters: Broadcaster[];
-    nationalRadioBroadcasters: Broadcaster[];
-    nationalOttBroadcasters: Broadcaster[];
-    homeTvBroadcasters: Broadcaster[];
-    homeRadioBroadcasters: Broadcaster[];
-    homeOttBroadcasters: Broadcaster[];
-    awayTvBroadcasters: Broadcaster[];
-    awayRadioBroadcasters: Broadcaster[];
-    awayOttBroadcasters: Broadcaster[];
-  };
   homeTeam: {
     teamId: number;
     teamName: string;
     teamCity: string;
     teamTricode: string;
-    teamSlug: string;
-    wins: number;
-    losses: number;
     score: number;
-    seed: number;
   };
   awayTeam: {
     teamId: number;
     teamName: string;
     teamCity: string;
     teamTricode: string;
-    teamSlug: string;
-    wins: number;
-    losses: number;
     score: number;
-    seed: number;
   };
-  pointsLeaders: Array<{
-    personId: number;
-    firstName: string;
-    lastName: string;
-    teamId: number;
-    teamCity: string;
-    teamName: string;
-    teamTricode: string;
-    points: number;
-  }>;
 }
 
 interface GameDate {
@@ -119,8 +59,8 @@ export class NbaOfficialGameRpository implements GameRepository {
     const response = await fetch(url as string);
     const result = await response.json();
 
-    const games = result.leagueSchedule.gameDates.flatMap(
-      (gameDate: GameDate) =>
+    const games: Game[] = result.leagueSchedule.gameDates.flatMap(
+      (gameDate: GameDate): Game[] =>
         gameDate.games
           .filter(
             (game: GameDetail) =>
@@ -141,6 +81,7 @@ export class NbaOfficialGameRpository implements GameRepository {
               homeTeamId: homeTeam.teamId.toString(),
               homeTeamPeriods: [],
               homeTeamScore: homeTeam.score,
+              date: '',
               startTime: game.gameDateTimeUTC,
             };
           })
