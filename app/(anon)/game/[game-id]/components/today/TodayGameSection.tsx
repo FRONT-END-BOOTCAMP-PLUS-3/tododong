5df71b3ffadb6abb9c13e9ad48947437bc0fe2a0
jisooyoon <yoonmallang@gmail.com>
2025-03-26 14:34:36 +0900

    ✨ feat: 게임 페이지 스켈레톤 UI 추가

--- app/(anon)/game/[game-id]/components/today/TodayGameSection.tsx
+++ app/(anon)/game/[game-id]/components/today/TodayGameSection.tsx
@@ -17,6 +17,7 @@ import { NavigationOptions } from 'swiper/types';
 import TodayGameCard from './TodayGameCard';
 
 const TodayGameSection = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isLastSlide, setIsLastSlide] = useState(() => false);
   const [isFirstSlide, setIsFirstSlide] = useState(() => true);
   const prevRef = useRef<HTMLButtonElement | null>(null);
@@ -28,7 +29,9 @@ const TodayGameSection = () => {
     const fetchScheduledGames = async () => {
       try {
         const response = await fetcher<ScheduledGameDto[]>(
-          `${process.env.NEXT_PUBLIC_API_URL}/schedule/${dayjs().format('YYYY-MM-DD')}`
+          `${process.env.NEXT_PUBLIC_API_URL}/schedule/${dayjs().format('YYYY-MM-DD')}`,
+          {},
+          setIsLoading
         );
         // fetch가 끝난 데이터
         setTodayGames([...response]);
@@ -55,69 +58,74 @@ const TodayGameSection = () => {
         <br />
         경기 일정
       </h2>
-
-      {shouldShowNavigation && (
-        <button
-          ref={prevRef}
-          type="button"
-          className={styles.customPrev}
-          disabled={isFirstSlide}
-        >
-          <Icon id="left" width={6.55} height={11.15} />
-        </button>
-      )}
-      <Swiper
-        modules={[Navigation]}
-        navigation={{
-          prevEl: prevRef.current,
-          nextEl: nextRef.current,
-        }}
-        onSwiper={(swiper) => {
-          setTimeout(() => {
-            if (
-              prevRef.current &&
-              nextRef.current &&
-              typeof swiper.params.navigation !== 'boolean'
-            ) {
-              const navigationParams = swiper.params
-                .navigation as NavigationOptions;
-              navigationParams.prevEl = prevRef.current;
-              navigationParams.nextEl = nextRef.current;
-              swiper.navigation.init();
-              swiper.navigation.update();
-            }
-          });
-        }}
-        onSlideChange={(swiper) => {
-          setIsLastSlide(swiper.isEnd);
-          setIsFirstSlide(swiper.isBeginning);
-        }}
-        slidesPerView={5} // 한 번에 보이는 카드 개수
-        slidesPerGroup={3} // 내비게이션 클릭 시 이동할 개수
-        spaceBetween={16}
-        centerInsufficientSlides // 마지막에 빈 공간 없이 정렬
-        className={`swiper ${todayGames.length <= 5 ? 'limited-swiper' : 'full-swiper'}`}
-      >
-        {todayGames.map((data, index) => (
-          <SwiperSlide key={data?.gameId ?? index}>
-            <TodayGameCard
-              gameId={data?.gameId}
-              gameStatus={data?.gameStatus}
-              startTime={data?.startTime}
-              teams={data?.teams}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      {shouldShowNavigation && (
-        <button
-          ref={nextRef}
-          type="button"
-          className={styles.customNext}
-          disabled={isLastSlide}
-        >
-          <Icon id="right" width={6.55} height={11.15} />
-        </button>
+      {isLoading ? (
+        <div className={styles.loading} />
+      ) : (
+        <>
+          {shouldShowNavigation && (
+            <button
+              ref={prevRef}
+              type="button"
+              className={styles.customPrev}
+              disabled={isFirstSlide}
+            >
+              <Icon id="left" width={6.55} height={11.15} />
+            </button>
+          )}
+          <Swiper
+            modules={[Navigation]}
+            navigation={{
+              prevEl: prevRef.current,
+              nextEl: nextRef.current,
+            }}
+            onSwiper={(swiper) => {
+              setTimeout(() => {
+                if (
+                  prevRef.current &&
+                  nextRef.current &&
+                  typeof swiper.params.navigation !== 'boolean'
+                ) {
+                  const navigationParams = swiper.params
+                    .navigation as NavigationOptions;
+                  navigationParams.prevEl = prevRef.current;
+                  navigationParams.nextEl = nextRef.current;
+                  swiper.navigation.init();
+                  swiper.navigation.update();
+                }
+              });
+            }}
+            onSlideChange={(swiper) => {
+              setIsLastSlide(swiper.isEnd);
+              setIsFirstSlide(swiper.isBeginning);
+            }}
+            slidesPerView={5} // 한 번에 보이는 카드 개수
+            slidesPerGroup={3} // 내비게이션 클릭 시 이동할 개수
+            spaceBetween={16}
+            centerInsufficientSlides // 마지막에 빈 공간 없이 정렬
+            className={`swiper ${todayGames.length <= 5 ? 'limited-swiper' : 'full-swiper'}`}
+          >
+            {todayGames.map((data, index) => (
+              <SwiperSlide key={data?.gameId ?? index}>
+                <TodayGameCard
+                  gameId={data?.gameId}
+                  gameStatus={data?.gameStatus}
+                  startTime={data?.startTime}
+                  teams={data?.teams}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          {shouldShowNavigation && (
+            <button
+              ref={nextRef}
+              type="button"
+              className={styles.customNext}
+              disabled={isLastSlide}
+            >
+              <Icon id="right" width={6.55} height={11.15} />
+            </button>
+          )}
+        </>
       )}
     </section>
   );
--- app/(anon)/game/[game-id]/layout.tsx
+++ app/(anon)/game/[game-id]/layout.tsx
@@ -7,6 +7,8 @@ import GamePageNav from './components/game-page-nav/GamePageNav';
 import GameStatusSection from './components/game-status/GameStatusSection';
 import TodayGameSection from './components/today/TodayGameSection';
 import styles from './layout.module.scss';
+import { Suspense } from 'react';
+import Loading from './components/loading/loading';
 
 // 여기서 쿠키에서 토큰을 꺼내서 props로 유저 정보 전달
 // 클라이언트에서는 쿠키를 쓸 수 없음
@@ -56,21 +58,23 @@ const GameLayout = async ({
 
   return (
     <>
-      <TodayGameSection />
-      <div className={styles.container}>
-        <div className={styles.mainInfo}>
-          <GameStatusSection gameInfo={gameInfo} />
-          <div className={styles.gamePageMain}>
-            <GamePageNav gameId={gameId} />
-            {children}
+      <Suspense fallback={<Loading />}>
+        <TodayGameSection />
+        <div className={styles.container}>
+          <div className={styles.mainInfo}>
+            <GameStatusSection gameInfo={gameInfo} />
+            <div className={styles.gamePageMain}>
+              <GamePageNav gameId={gameId} />
+              {children}
+            </div>
           </div>
+          <ChatSection
+            userInfo={userInfo}
+            gameId={gameId}
+            gameState={gameInfo.gameStatus}
+          />
         </div>
-        <ChatSection
-          userInfo={userInfo}
-          gameId={gameId}
-          gameState={gameInfo.gameStatus}
-        />
-      </div>
+      </Suspense>
     </>
   );
 };
