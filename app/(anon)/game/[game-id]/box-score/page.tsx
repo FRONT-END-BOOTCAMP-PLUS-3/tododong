717090119c90dbfc1864e043f10feab347dba816
seungsu-K <kss095@naver.com>
2025-03-28 09:46:28 +0900

    ♻️ refactor : 박스스코어 데이터를 react-query로 패치

--- app/(anon)/game/[game-id]/box-score/page.tsx
+++ app/(anon)/game/[game-id]/box-score/page.tsx
@@ -7,39 +7,52 @@ import { useEffect, useState } from 'react';
 import { fetcher } from '@/utils';
 import { BoxscoreDto } from '@/application/usecases/game/box-score/dto/boxscoreDto';
 import Loader from '@/components/loader/Loader';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchBoxscore = async (gameId: string | string[]) => {
+  const id = Array.isArray(gameId) ? gameId[0] : gameId;
+
+  return fetcher<BoxscoreDto>(`/api/game/${id}/box-score`);
+};
 
 const BoxScore = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(() => true);
-  const [boxscoreData, setBoxscoreData] = useState<BoxscoreDto>();
-
   const params = useParams();
   const gameId = params['game-id'];
 
+  const [queryOptions, setQueryOptions] = useState<{
+    staleTime: number;
+    refetchInterval: number | false;
+  }>({ staleTime: 600000, refetchInterval: false });
+
+  const {
+    data: boxscoreData,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['boxscore', gameId],
+    queryFn: () => fetchBoxscore(gameId!),
+    staleTime: queryOptions.staleTime,
+    refetchInterval: queryOptions.refetchInterval,
+    enabled: !!gameId,
+  });
+
   useEffect(() => {
-    if (!gameId) return;
-
-    const fetchBoxscore = async () => {
-      try {
-        const response = await fetcher<BoxscoreDto>(
-          `/api/game/${gameId}/box-score`,
-          {},
-          setIsLoading
-        );
-        setBoxscoreData(response);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchBoxscore();
-  }, [gameId]);
+    if (boxscoreData?.game.status === 'scheduled') {
+      setQueryOptions({ staleTime: 0, refetchInterval: false });
+    } else if (boxscoreData?.game.status === 'live') {
+      setQueryOptions({ staleTime: 10000, refetchInterval: 10000 });
+    } else {
+      setQueryOptions({ staleTime: 600000, refetchInterval: false });
+    }
+  }, [boxscoreData?.game.status]);
 
   if (isLoading || !boxscoreData) return <Loader />;
-  if (boxscoreData.game.status === 'scheduled') {
+  if (!boxscoreData || error)
+    return <div className={styles.statusInfo}>추후 업데이트 예정입니다.</div>;
+  if (boxscoreData.game.status === 'scheduled')
     return (
-      <div className={styles.scheduledGame}>경기 시작 후 업데이트 됩니다.</div>
+      <div className={styles.statusInfo}>경기 시작 후 업데이트 됩니다.</div>
     );
-  }
 
   return (
     <div className={styles.boxScoreContainer}>
