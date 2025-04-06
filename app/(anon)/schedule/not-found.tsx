6a2155ff3e00531dd8cb1c2856ea8ead8fc51f28
jisooyoon <yoonmallang@gmail.com>
2025-04-06 20:00:44 +0900

    ✨ feat: schedule 404 페이지 추가

---
+++ app/(anon)/schedule/not-found.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import styles from './not-found.module.scss';
+
+export default function NotFound() {
+  return (
+    <main className={styles.page}>
+      <div className={styles.contents}>
+        <h1 className={styles.title}>404 ERROR</h1>
+        <p>페이지를 찾을 수 없습니다.</p>
+        <div>
+          <p>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.</p>
+          <p>입력하신 주소가 정확한지 다시 한 번 확인해 주세요.</p>
+        </div>
+      </div>
+
+      <a href="/" aria-label="홈으로 이동" className={styles.homeButton}>
+        홈으로 이동하기
+      </a>
+    </main>
+  );
+}
